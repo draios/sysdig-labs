c49121d0ba47e78e02fec8f3480804d26ce177dc
davideschiera <davide.schiera@gmail.com>
2018-10-25 17:09:21 -0700

    Few fixes

--- image-scanning-notification/index.js
+++ image-scanning-notification/index.js
@@ -13,14 +13,14 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
-"use strict";
+'use strict';
 
-const axios = require("axios");
-const Handlebars = require("Handlebars");
-const nodemailer = require("nodemailer");
-const moment = require("moment");
-const minimist = require("minimist");
-const fs = require("fs");
+const axios = require('axios');
+const Handlebars = require('Handlebars');
+const nodemailer = require('nodemailer');
+const moment = require('moment');
+const minimist = require('minimist');
+const fs = require('fs');
 
 main(process.argv.slice(2));
 
@@ -28,19 +28,19 @@ async function main(args) {
   //
   // Get parameters
   //
-  console.info("Reading parameters...");
+  console.info('Reading parameters...');
   const params = parseParameters(args);
 
   //
   // Fetch data
   //
-  console.info("Fetching data...");
+  console.info('Fetching data...');
 
   const apiAdapter = axios.create({
     baseURL: params.sysdigUrl,
     timeout: 30000,
     headers: {
-      "X-Sysdig-Product": "SDS",
+      'X-Sysdig-Product': 'SDS',
       Authorization: `Bearer ${params.sysdigToken}`
     }
   });
@@ -50,62 +50,54 @@ async function main(args) {
   //
   // Define template context
   //
-  console.info("Defining email context...");
+  console.info('Defining email context...');
   const context = defineContext(data, params);
 
   //
   // Mix data and template
   //
-  console.info("Creating email content...");
+  console.info('Creating email content...');
   initializeRendering();
-  const subject = await buildContent(
-    context,
-    "./templates/image-scan-result-subject.hbs",
-    params
-  );
-  const text = await buildContent(
-    context,
-    "./templates/image-scan-result-text.hbs",
-    params
-  );
+  const subject = await buildContent(context, './templates/image-scan-result-subject.hbs', params);
+  const text = await buildContent(context, './templates/image-scan-result-text.hbs', params);
   const html = await buildContent(
     {
       ...context,
-      textSummary: text.replace(/\n/g, ""),
-      subject: subject.replace(/\n/g, "")
+      textSummary: text.replace(/\n/g, ''),
+      subject: subject.replace(/\n/g, '')
     },
-    "./templates/image-scan-result-html.hbs",
+    './templates/image-scan-result-html.hbs',
     params
   );
 
   //
   // Send email
   //
-  console.info("Sending email...");
+  console.info('Sending email...');
   await sendEmail({ subject, textContent: text, htmlContent: html }, params);
 
-  console.info("All done!");
+  console.info('All done!');
 }
 
 function parseParameters(args) {
   const argv = minimist(args);
 
   return {
-    sysdigUrl: getParam(argv, "sysdig-url"),
-    sysdigToken: getParam(argv, "sysdig-token"),
-    assetsBaseUrl: "https://download.sysdig.com/assets/",
+    sysdigUrl: getParam(argv, 'sysdig-url'),
+    sysdigToken: getParam(argv, 'sysdig-token'),
+    assetsBaseUrl: 'https://download.sysdig.com/assets/',
 
-    recipients: Array.isArray(getParam(argv, "recipient"))
-      ? getParam(argv, "recipient")
-      : [getParam(argv, "recipient")],
-    sender: getParam(argv, "sender", "notifications@sysdig.com"),
+    recipients: Array.isArray(getParam(argv, 'recipient'))
+      ? getParam(argv, 'recipient')
+      : [getParam(argv, 'recipient')],
+    sender: getParam(argv, 'sender', 'notifications@sysdig.com'),
 
-    smtpHost: getParam(argv, "smtp-host"),
-    smtpPort: getParam(argv, "smtp-port", 25),
-    smtpUser: getParam(argv, "smtp-user"),
-    smtpPass: getParam(argv, "smtp-pass"),
+    smtpHost: getParam(argv, 'smtp-host'),
+    smtpPort: getParam(argv, 'smtp-port', 25),
+    smtpUser: getParam(argv, 'smtp-user'),
+    smtpPass: getParam(argv, 'smtp-pass'),
 
-    imageId: getParam(argv, "image")
+    imageId: getParam(argv, 'image')
   };
 
   function getParam(argv, name, defValue) {
@@ -138,9 +130,7 @@ async function fetchData(apiAdapter, params) {
   const vulnerabilitiesDataResponse = await apiAdapter.get(
     `/api/scanning/v1/anchore/images/${params.imageId}/vuln/os`
   );
-  const vulnerabilitiesData = parseVulnerabilitiesData(
-    vulnerabilitiesDataResponse.data
-  );
+  const vulnerabilitiesData = parseVulnerabilitiesData(vulnerabilitiesDataResponse.data);
 
   return {
     image: imageData,
@@ -170,31 +160,39 @@ async function fetchData(apiAdapter, params) {
     const scanResult = scanResultObj[Object.keys(scanResultObj)[0]];
 
     const rowHeaders = scanResult.result.header;
-    const rows = scanResult.result.rows.map(row => {
-      return {
-        gateAction: row[rowHeaders.indexOf("Gate_Action")],
-        trigger: row[rowHeaders.indexOf("Trigger")],
-        gate: row[rowHeaders.indexOf("Gate")],
-        output: row[rowHeaders.indexOf("Check_Output")],
-        isStop: row[rowHeaders.indexOf("Gate_Action")] === "stop",
-        isWarn: row[rowHeaders.indexOf("Gate_Action")] === "warn"
-      };
-    });
+    const rows = scanResult.result.rows
+      .map((row) => {
+        return {
+          gateAction: row[rowHeaders.indexOf('Gate_Action')],
+          trigger: row[rowHeaders.indexOf('Trigger')],
+          gate: row[rowHeaders.indexOf('Gate')],
+          output: row[rowHeaders.indexOf('Check_Output')],
+          isStop: row[rowHeaders.indexOf('Gate_Action')] === 'stop',
+          isWarn: row[rowHeaders.indexOf('Gate_Action')] === 'warn'
+        };
+      })
+      .sort((a, b) => {
+        const actions = ['stop', 'warn'];
+        const actionSort = actions.indexOf(a.gateAction) - actions.indexOf(b.gateAction);
+        if (actionSort !== 0) {
+          return actionSort;
+        }
+
+        return a.output.localeCompare(b.output);
+      });
 
     const policyId = latestScan.detail.result.matched_mapping_rule.policy_id;
-    const policy = latestScan.detail.policy.policies.find(
-      policy => policy.id === policyId
-    );
+    const policy = latestScan.detail.policy.policies.find((policy) => policy.id === policyId);
 
     return {
       status: latestScan.status,
-      isFail: latestScan.status === "fail",
-      isPass: latestScan.status === "pass",
+      isFail: latestScan.status === 'fail',
+      isPass: latestScan.status === 'pass',
       evaluatedAt: latestScan.last_evaluation,
       policy: parseScanResultPolicy(policy),
       rows,
-      stopRows: rows.filter(row => row.gateAction === "stop"),
-      warnRows: rows.filter(row => row.gateAction === "warn")
+      stopRows: rows.filter((row) => row.gateAction === 'stop'),
+      warnRows: rows.filter((row) => row.gateAction === 'warn')
     };
   }
 
@@ -208,14 +206,12 @@ async function fetchData(apiAdapter, params) {
     const vulnerabilities = data.vulnerabilities
       .map(parseVulnerability)
       .filter(
-        vulnerability =>
-          vulnerability.fix !== "None" &&
-          ["High", "Medium"].indexOf(vulnerability.severity) >= 0
+        (vulnerability) =>
+          vulnerability.fix !== 'None' && ['High', 'Medium'].indexOf(vulnerability.severity) >= 0
       )
       .sort((a, b) => {
-        const severities = ["High", "Medium"];
-        const severitySort =
-          severities.indexOf(a.severity) - severities.indexOf(b.severity);
+        const severities = ['High', 'Medium'];
+        const severitySort = severities.indexOf(a.severity) - severities.indexOf(b.severity);
         if (severitySort !== 0) {
           return severitySort;
         }
@@ -224,9 +220,9 @@ async function fetchData(apiAdapter, params) {
       });
 
     return {
-      list: vulnerabilities.slice(0, 5),
-      count: data.vulnerabilities.length,
-      hasMore: data.vulnerabilities.length > 5
+      list: vulnerabilities.slice(0, 20),
+      count: vulnerabilities.length,
+      hasMore: vulnerabilities.length > 20
     };
   }
 
@@ -260,8 +256,8 @@ async function buildContent(context, templateFilePath, params) {
 }
 
 function initializeRendering() {
-  Handlebars.registerHelper("timestampToString", function(timestamp) {
-    return moment(timestamp).format("MMMM Do YYYY, h:mm:ss a");
+  Handlebars.registerHelper('timestampToString', function(timestamp) {
+    return moment(timestamp).format('MMMM Do YYYY, h:mm:ss a');
   });
 }
 
@@ -279,24 +275,20 @@ async function sendEmail(content, params) {
   // setup email data with unicode symbols
   const mailOptions = {
     from: params.sender,
-    to: params.recipients.join(", "),
+    to: params.recipients.join(', '),
     subject: content.subject,
     text: content.textContent,
     html: content.htmlContent
   };
 
   // send mail with defined transport object
-  const result = await promisify(
-    transporter,
-    transporter.sendMail,
-    mailOptions
-  );
+  const result = await promisify(transporter, transporter.sendMail, mailOptions);
 
-  console.log("Message sent: %s", result.messageId);
+  console.log('Message sent: %s', result.messageId);
 }
 
 async function readFile(filePath) {
-  return promisify(fs, fs.readFile, filePath, "utf8");
+  return promisify(fs, fs.readFile, filePath, 'utf8');
 }
 
 async function promisify(target, fn, ...args) {
