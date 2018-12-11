3db388b7faaa1252b9a12197e79530a6d632942d
Kaizhe Huang <derek0405@gmail.com>
2018-12-11 11:21:26 -0800

    update image repo

--- image-scanning-admission-controller/scripts/clean.sh
+++ image-scanning-admission-controller/scripts/clean.sh
@@ -2,20 +2,5 @@
 
 set -eux
 
-if [[ -z "$ANCHORE_CLI_URL" ]]; then
-	echo '$ANCHORE_CLI_URL is empty or unset'
-	exit 1;
-fi;
-
-if [[ -z "$ANCHORE_CLI_USER" ]]; then
-	echo '$ANCHORE_CLI_USER is empty or unset'
-	exit 1;
-fi;
-
-if [[ -z "${ANCHORE_CLI_PASS+x}" ]]; then
-	echo '${ANCHORE_CLI_PASS} is unset';
-	exit 1;
-fi;
-
-sed 's@{{.ANCHORE_USER}}@'"${ANCHORE_CLI_USER}"'@; s@{{.ANCHORE_URL}}@'"${ANCHORE_CLI_URL}"'@' image-scan-admission-webhook.yaml | kubectl delete -f -
+kubectl delete -f image-scanning-admission-controller.yaml
 kubectl delete ValidatingWebhookConfiguration validating-webhook-configuration || true
