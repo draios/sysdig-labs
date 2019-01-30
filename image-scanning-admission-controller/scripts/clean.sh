aaf54d6c5a4476025f422f532466c9b8757c7203
Kaizhe Huang <derek0405@gmail.com>
2019-01-29 19:47:39 -0800

    fix clean script and add comment about default api scanning endpoint

--- image-scanning-admission-controller/scripts/clean.sh
+++ image-scanning-admission-controller/scripts/clean.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-kubectl delete -f image-scanning-admission-controller.yaml
+kubectl delete -f image-scanning-admission-controller.yaml || true
 kubectl delete ValidatingWebhookConfiguration validating-webhook-configuration || true
